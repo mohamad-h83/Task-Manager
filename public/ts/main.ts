ae9d8ec040ae154be009194ee49af9b0ecf23244
mohamad-h83 <mheirany8383@gmail.com>
2025-01-18 15:30:39 +0330

    Update main.ts

--- public/ts/main.ts
+++ public/ts/main.ts
@@ -1,19 +1,29 @@
 let userinputs: {title: string, description: string, completed: boolean,dueDate:number}[] = [];
 
-const form = document.getElementById('form') as HTMLFormElement;
+const form = document.getElementById('myform') as HTMLFormElement;
 const title = document.getElementById("title")as HTMLFormElement;
 const description = document.getElementById("description")as HTMLFormElement;
 const completed = document.getElementById("completed")as HTMLFormElement;
 const dueDate = document.getElementById("dueDate")as HTMLFormElement;
-
+ 
 form.addEventListener('submit',(event) => {
   event.preventDefault();
-})
-
-const userinput= {title:title.value , description:description.value,completed:completed.value,dueDate:dueDate.value}
+  const userinput= {title:title.value, description:description.value,completed:completed.value,dueDate:dueDate.value}
 userinputs.push(userinput);
-console.log(userinputs);
 
+userinputs.forEach((userinputs) => {
+  if (completed.value === true) {
+    console.log(
+      `completed task: title:${userinputs.title} -desciption: ${userinputs.description} - Completed: ${userinputs.completed} - Due Date: ${userinputs.dueDate}`
+    );
+  }
+  else{
+    console.log(
+      `uncompleted task: title:${userinputs.title} -desciption: ${userinputs.description} - Completed: ${userinputs.completed} - Due Date: ${userinputs.dueDate}`
+    );
+  }
+});
+})
 
 
 
