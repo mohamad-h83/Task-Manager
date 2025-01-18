319ab9d3fdce674601c9c8ca48e0d605929e48d6
mohamad-h83 <mheirany8383@gmail.com>
2025-01-18 15:32:43 +0330

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,26 +1,21 @@
-let userinputs: {title: string, description: string, completed: boolean,dueDate:number}[] = [];
-
-const form = document.getElementById('myform') as HTMLFormElement;
-const title = document.getElementById("title")as HTMLFormElement;
-const description = document.getElementById("description")as HTMLFormElement;
-const completed = document.getElementById("completed")as HTMLFormElement;
-const dueDate = document.getElementById("dueDate")as HTMLFormElement;
- 
-form.addEventListener('submit',(event) => {
-  event.preventDefault();
-  const userinput= {title:title.value, description:description.value,completed:completed.value,dueDate:dueDate.value}
-userinputs.push(userinput);
-
-userinputs.forEach((userinputs) => {
-  if (completed.value === true) {
-    console.log(
-      `completed task: title:${userinputs.title} -desciption: ${userinputs.description} - Completed: ${userinputs.completed} - Due Date: ${userinputs.dueDate}`
-    );
-  }
-  else{
-    console.log(
-      `uncompleted task: title:${userinputs.title} -desciption: ${userinputs.description} - Completed: ${userinputs.completed} - Due Date: ${userinputs.dueDate}`
-    );
-  }
+var userinputs = [];
+var form = document.getElementById('myform');
+var title = document.getElementById("title");
+var description = document.getElementById("description");
+var completed = document.getElementById("completed");
+var dueDate = document.getElementById("dueDate");
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var userinput = { title: title.value, description: description.value, completed: completed.value, dueDate: dueDate.value };
+    userinputs.push(userinput);
+    userinputs.forEach(function (userinputs) {
+        if (completed.value === true) {
+            console.log("completed task: title:".concat(userinputs.title, " -desciption: ").concat(userinputs.description, " - Completed: ").concat(userinputs.completed, " - Due Date: ").concat(userinputs.dueDate));
+        }
+        else {
+            console.log("uncompleted task: title:".concat(userinputs.title, " -desciption: ").concat(userinputs.description, " - Completed: ").concat(userinputs.completed, " - Due Date: ").concat(userinputs.dueDate));
+        }
+    });
+});
 });
 })
